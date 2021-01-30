e810ec160cee55a69d0da4522e867d0499863d0a
AnshSinha9140 <anshsinha8604@gmail.com>
2021-01-30 14:11:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shareapp/Main_sharescreen.java
@@ -0,0 +1,57 @@
+package com.example.shareapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class Main_sharescreen extends AppCompatActivity {
+
+    private Button main_scan_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sharescreen);
+
+        main_scan_btn = findViewById(R.id.main_scan_btn);
+        main_scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanCode();
+            }
+        });
+    }
+
+    private void scanCode() {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this
+        );
+        intentIntegrator.setCaptureActivity(CaptureAct.class);
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intentIntegrator.setPrompt("Scanning Code");
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result.getContents() != null) {
+
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(this, "No result found", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
